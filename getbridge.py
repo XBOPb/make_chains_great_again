13f53dcd40d52104f1ed310131eead39d76a4911
Your Name <you@example.com>
2024-12-19 00:42:35 +0300

    config updating mech

--- getbridge.py
+++ getbridge.py
@@ -1,6 +1,11 @@
 import requests
 import subprocess
 
+config_template = 'config.txt'
+torrc_config = '/etc/tor/torrc'
+torrc_backup = '/etc/tor/torrc_backup'
+
+
 def request_bridges():
     bridges = []
     params = {
@@ -10,31 +15,32 @@ def request_bridges():
     text = response.text
     for line in text.split('\n'):
         if line.strip().startswith('obfs4'):
-            bridges.append(line)
+            bridges.append(line.strip().replace(' <br/>', ''))
     return bridges
 
 
-def read_config():
-    with open('config.txt') as file:
+def read_template():
+    with open(config_template) as file:
         config_text = file.read()
     return config_text
     
     
-def form_config_text(config, bridges):
-    config_text = config.replace('bridge_1', bridges[0]).replace('bridge_2', bridges[1])
+def form_config_text(config_template, bridges):
+    config_text = config_template.replace('bridge_1', bridges[0]).replace('bridge_2', bridges[1])
     return config_text
     
     
 def modify_tor_config(config_text):
-    # subprocess.run('./edit_tor_config.sh')
-    # subprocess.getoutput(f'chmod o+w /etc/tor/torrc')
-    # subprocess.getoutput(f'{config_text} >> /etc/tor/torrc')
-    # subprocess.getoutput(f'chmod o-w /etc/tor/torrc')
-    
+    subprocess.getoutput(f'echo "{config_text}" >> {torrc_config}')
+
+
+def backup_existing_config():
+    subprocess.getoutput(f'cp {torrc_config} {torrc_backup}')
+
 
 if __name__=='__main__':
-    config = read_config()
+    config_template = read_template()
     bridges = request_bridges()
-    config_text = form_config_text(config, bridges)
-    print(f'adding {config_text}')
+    config_text = form_config_text(config_template, bridges)
+    backup_existing_config()
     modify_tor_config(config_text)
